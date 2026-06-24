6235311fb22407b85c312a5ac586cf11bb7f952f
ongmonkorn <ongtama.ong@gmail.com>
2026-06-24 15:47:43 +0700

    Update users.js

--- backend/web-service/controller/users.js
+++ backend/web-service/controller/users.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'เกิดข้อผิดพลาดในการเข้าสู่ระบบ' });
+        res.status(500).json({ message: 'ควยยยยย' });
     }
 };
 
